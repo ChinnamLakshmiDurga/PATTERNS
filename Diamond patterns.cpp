eaba12113782562abb0a6d3ace84ba9a340ff550
Chinnam Lakshmi Durga <90793864+ChinnamLakshmiDurga@users.noreply.github.com>
2021-10-09 15:52:33 +0530

    Using stars

---
+++ Diamond patterns.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   for(int i=1;i<=n;i++){
+       int j;
+       for(j=1;j<=n;j++){
+           if(j<=n-i){
+               cout<<" ";
+           }
+           else{
+               cout<<"* ";
+           }
+       }
+       cout<<endl;
+   }
+   for(int i=n;i>=1;i--){
+       int j;
+       for(j=1;j<=n;j++){
+           if(j<=n-i){
+               cout<<" ";
+           }
+           else{
+               cout<<"* ";
+           }
+       }
+       cout<<endl;
+   }
+   
+   return 0;
+}
+
+
+Input:
+5
+
+Output:
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
