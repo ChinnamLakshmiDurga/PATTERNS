3e5b0f6bef40aba1060348ac3585a13e6dfa0074
Chinnam Lakshmi Durga <90793864+ChinnamLakshmiDurga@users.noreply.github.com>
2021-10-14 19:25:19 +0530

    Using stars

--- Hollow Traingle.cpp
+++ Hollow Traingle.cpp
@@ -1,14 +1,6 @@
 #include<iostream>
 using namespace std;
 
-int fact(int num){
-    int prod=1;
-    for(int i=2;i<=num;i++){
-        prod*=i;
-    }
-    return prod;
-}
-
 int main(){
     int n;
     cin>>n;
